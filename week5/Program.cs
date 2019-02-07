b897b51100d9bb9c35dc92992a289fe1ef21c82a
visualstudio <visualstudio@230-PC26>
2019-02-07 14:09:53 -0500

    bhvghj

--- week5/Program.cs
+++ week5/Program.cs
@@ -6,7 +6,41 @@ namespace week5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
+
+        class TestQuestion2
+        {
+            public int myFavoriteVariable = 0;
+            public void PlayingWithForLoops()
+            {
+
+
+                // write For Loop to Add 10 numbers
+                for (; MyMethod();)
+                {
+
+                    if (myFavoriteVariable > 10)
+
+                    {
+                        Console.WriteLine(" I am so out of here!!  ");
+                        Console.ReadLine();
+                        break;
+                    }
+
+
+
+                    Console.WriteLine(" Oh no i have to go through this again  ");
+
+                    
+            }
+
+            public bool MyMethod()
+            {
+                return true;
+            }
+        }
+
+
     }
 }
