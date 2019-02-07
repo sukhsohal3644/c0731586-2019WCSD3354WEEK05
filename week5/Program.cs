f7e556d37c52e6ecbcb75212875bd7e285fe5160
visualstudio <visualstudio@230-PC26>
2019-02-07 15:46:10 -0500

    3:40 commit

--- week5/Program.cs
+++ week5/Program.cs
@@ -48,26 +48,60 @@ namespace week5
 
 
         }
+        public dog()
+        {
+        }
         public string dog_name ;
         public string dog_breed;
         public dog next_dog;
-        public dog previous_dog;
+        public dog prev_dog;
 
 
     }
     class birthday_party
     {
         
-        public dog fifi;
+      
         public dog peanut;
+        public dog fifi;
         public dog clarence;
         public dog roy;
-        public dog tomy;
+       
         public dog head;
         public dog tail;
-        public dog temp;
+        public dog temporary;
 
+        public void setupPartyList()
+        {
+            peanut = new dog("Peanut", "Bichon");
+            fifi = new dog("fifi", "Poodle ");
+            clarence = new dog("Clarence", "German Sheppard");
+            roy = new dog("Roy", "Beagle");
+
+
+            peanut.prev_dog = null;
+            peanut.next_dog = fifi;
+            fifi.prev_dog = peanut;
+            fifi.next_dog = clarence;
+            clarence.prev_dog = fifi;
+            clarence.next_dog = roy;
+            roy.prev_dog = clarence;
+            roy.next_dog = null;
+            head = peanut;
+            tail = roy;
+
+        }
+        public string printPartyList()
+        {
+            string inviteList = "*--";
+            temporary = head;
+            while (temporary.next_dog != null)
+            {
+                inviteList += temporary.dog_name + " * --- * ";
+            }
+
+            return inviteList;
+
+        }
 
     }
-    
-}
